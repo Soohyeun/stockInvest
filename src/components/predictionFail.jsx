f57ad279c7929580bcf68b4269f5b998b9d4e6d5
Soohyeun <soohyeunp@gmail.com>
2024-07-15 02:44:12 -0700

    Fix a bug

--- src/components/predictionFail.jsx
+++ src/components/predictionFail.jsx
@@ -4,7 +4,7 @@ export function PredictionFail() {
   return (
     <div className="mt-10">
       <h3 className="text-5xl font-bold text-gray-500 text-center">
-        The symbol you entered was not found :'(
+        The symbol you entered was not found :(
       </h3>
       <div className="pt-10 text-xl text-gray-500 text-center">
         Please enter another stock symbol.
