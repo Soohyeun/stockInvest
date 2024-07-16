f9ed348a0d963e8f603fc0bea646e01bb5d038f2
Soohyeun <soohyeunp@gmail.com>
2024-07-15 23:19:41 -0700

    Undo loading

--- src/components/ui/loading.jsx
+++ src/components/ui/loading.jsx
@@ -1,14 +1,7 @@
 export function Loading() {
   return (
-    <div>
-      <div className="loading pt-3 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h3 className="text-2xl font-bold text-center pb-10 text-slate-500">
-          Our predictive model is currently undergoing training. Please hold on.
-        </h3>
-        <div className="flex items-center justify-center">
-          <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-gray-600" />
-        </div>
-      </div>
+    <div className="loading pt-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-gray-600" />
     </div>
   );
 }
